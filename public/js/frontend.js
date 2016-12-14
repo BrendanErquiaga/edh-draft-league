4e1a5bef264851f1ede985e53a30c0f86c1b4e4f
Andrew Maul <amaul@ops-frontend-2.local>
2016-12-14 15:20:34 -0700

    Sets up nav and other fixes

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -11,11 +11,12 @@ function simpleAccordions() {
 // Smooth scrolling on internal (same-page) links
 function smoothScroll() {
 	$('a[href*="#"]:not([href="#"])').click(function() {
+		var navHeight = $('.nav').height();
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 			if (target.length) {
-				target = target.offset().top + window.scrollY;
+				target = target.offset().top + window.scrollY - navHeight;
 				$('html, body').animate({
 					scrollTop: target
 				}, 1000);
@@ -25,42 +26,8 @@ function smoothScroll() {
 	});
 }
 
-// Sticky Nav with debounce
-function stickyNav() {
-	// Hides sticky nav; if JS is disabled it will still be there
-	$('.stickynav').hide();
-
-	// Debounce JS for scroll events
-	function debounce(func, wait, immediate) {
-		var timeout;
-		return function() {
-			var context = this, args = arguments;
-			var later = function() {
-				timeout = null;
-				if (!immediate) func.apply(context, args);
-			};
-			var callNow = immediate && !timeout;
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
-			if (callNow) func.apply(context, args);
-		};
-	};
-	var myEfficientFn = debounce(function() {
-		if (($('a[name="Intro"]').offset().top) < 0){
-			$('.stickynav').fadeIn('fast');
-		} else {
-			$('.stickynav').fadeOut('fast');
-		}
-	}, 100);
-	// Scroll event handler, passes to debounce
-	if($('body').find('div.stickynav').length){
-		window.addEventListener('scroll', myEfficientFn);
-	}
-}
-
 // Document Ready
 $(document).ready(function(){
 	simpleAccordions();
-	stickyNav();
 	smoothScroll();
 });
